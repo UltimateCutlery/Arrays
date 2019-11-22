1ed39178a720535ad1e8b32b8e563db6f4eedb14
UltimateCutlery <57415618+UltimateCutlery@users.noreply.github.com>
2019-11-22 21:48:40 +0000

    Array_2

--- ArraysShoppingTask.js
+++ ArraysShoppingTask.js
@@ -5,13 +5,6 @@ var fruitVegShoppingList = [];
 
 // Code Below
 
-var shoppingList = ["Eggs","Apples","Bread","Milk","Carrots","Potatoes","Chicken"] //the shopping list
-var fruitVegList = ["Apples","Apricot","Artichoke","Carrots","Cabbage","Onion","Oranges","Potatoes"] //the list of accepted fruits and vegetables
-
-var fruitVegShoppingList = [];
-
-// Code Below
-
 for ( var i = 0; i <= [shoppingList.length - 1]; i++ ){
 
     for (var j = 0; j <= [fruitVegList.length - 1]; j++ ){
